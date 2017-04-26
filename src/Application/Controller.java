062ac45be758416160a87a74c44ab9bfd926d13d
seba0830 <sebastian_boerloes@hotmail.com>
2017-04-26 12:38:50 +0200

    ujiij

--- src/Application/Controller.java
+++ src/Application/Controller.java
@@ -4,4 +4,21 @@ package Application;
  * Created by LPNielsen on 26-Apr-17.
  */
 public class Controller {
+
+    private void createCustomer(String ){
+
+    }
+    asdasdas
+    asdasdasas
+
+    public asdasdas getAsdasdasas() {
+        as
+                asd
+                asd
+                        as
+    }
+
+    private void createBooking(){
+
+    }
 }
